4e896eb497fb2dd7bd1c76fd4ecf66861978c3eb
antonpavlov <anton.it.pavlov@gmail.com>
2018-02-14 20:05:24 +0300

    урок 2

--- src/main/kotlin/ru/pavlov/smlr/controllers/RedirectController.kt
+++ src/main/kotlin/ru/pavlov/smlr/controllers/RedirectController.kt
@@ -1,8 +1,10 @@
 package ru.pavlov.smlr.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import ru.pavlov.smlr.service.KeyMapperService
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -11,14 +13,33 @@ import javax.servlet.http.HttpServletResponse
 class RedirectController {
     private val HEADER_NAME = "Location"
     private val HEADER_VALUE = "http://www.eveonline.com"
+
+    @Autowired
+    lateinit var service: KeyMapperService
+
     @RequestMapping
     fun redirect(@PathVariable("key") key: String, response: HttpServletResponse) {
-        if (key == "aaaaaa") {
-            response.setHeader(HEADER_NAME, HEADER_VALUE)
-            response.status = 302
-        } else {
-            response.status = 404
+        val resualt = service.getLink(key)
+
+        when (resualt) {
+            is KeyMapperService.Get.Link -> {
+                response.setHeader(HEADER_NAME, resualt.link)
+                response.status = 302
+            }
+            is KeyMapperService.Get.LinkNotFound ->{
+                response.status = 404
+            }
         }
+
+
+//        while (resual) {
+//            if (key == "aaaaaa") {
+//                response.setHeader(HEADER_NAME, HEADER_VALUE)
+//                response.status = 302
+//            } else {
+//                response.status = 404
+//            }
+//        }
 
     }
 }
--- src/main/kotlin/ru/pavlov/smlr/service/DefaultKeyMapperService.kt
+++ src/main/kotlin/ru/pavlov/smlr/service/DefaultKeyMapperService.kt
@@ -1,7 +1,9 @@
 package ru.pavlov.smlr.service
 
+import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
+@Component
 class DefaultKeyMapperService : KeyMapperService {
 
     private val map: MutableMap<String, String> = ConcurrentHashMap()
